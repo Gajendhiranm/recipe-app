9eb5436e0231e7d1f2b5a13f033127d2afd28d4f
Gajendhiran-predigle <gajendhiran.mohan@predigle.com>
2024-01-21 14:18:14 +0530

    home link added

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -4,9 +4,8 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import "./Navbar.css";
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import { Link } from "react-router-dom"; 
-
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -23,19 +22,24 @@ export default function Navbar() {
             alignItems="center"
             className="box-container"
           >
-            <img
-              src="./images/recipe-logo.png"
-              height={50}
-              alt='burger-icon'
-            />
+            <Link to="/home">
+              <img
+                src="./images/recipe-logo.png"
+                height={50}
+                alt="burger-icon"
+              />
+            </Link>
+
             <Box display="flex">
-            
-              <Link to='/favorites'>
-              <Button variant="outlined"  startIcon={<FavoriteBorderOutlinedIcon />} className="Favorities-btn">
-                Favorites
-              </Button>
+              <Link to="/favorites">
+                <Button
+                  variant="outlined"
+                  startIcon={<FavoriteBorderOutlinedIcon />}
+                  className="Favorities-btn"
+                >
+                  Favorites
+                </Button>
               </Link>
-             
             </Box>
           </Box>
         </Toolbar>
